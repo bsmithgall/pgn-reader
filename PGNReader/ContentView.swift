cd206d179241501e2a6930f74ba2d1216e37b022
Ben Smithgall <ben.smithgall@gmail.com>
2020-11-09 17:09:44 -0500

    Initial Commit

---
+++ PGNReader/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  PGNReader
+//
+//  Created by Ben Smithgall on 11/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
